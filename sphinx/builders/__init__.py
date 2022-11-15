046dd60d88e51e1aa62a7c62f47d04a357d47299
Schulz Stefan <stefan.schulz@itemis.com>
2022-11-15 20:26:46 +0100

    Remove warmup

--- sphinx/builders/__init__.py
+++ sphinx/builders/__init__.py
@@ -588,15 +588,6 @@ class Builder:
                 self.env.events.emit('doctree-process', doctree, docname)
                 self.write_doc(docname, doctree)
 
-        # warm up caches/compile templates using the first document
-        firstname, docnames = docnames[0], docnames[1:]
-        self.app.phase = BuildPhase.RESOLVING
-        doctree = self.env.get_and_resolve_doctree(firstname, self)
-        self.env.events.emit('doctree-process', doctree, firstname)
-        self.app.phase = BuildPhase.WRITING
-        self.write_doc_serialized(firstname, doctree)
-        self.write_doc(firstname, doctree)
-
         tasks = ParallelTasks(nproc)
         chunks = make_chunks(docnames, nproc)
 
