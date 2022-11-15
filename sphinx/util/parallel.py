81dd60473c99cc63ce5982e0dae2317c5a57c5e7
Schulz Stefan <stefan.schulz@itemis.com>
2022-11-15 13:49:21 +0100

    Set maxbatch to 1000

--- sphinx/util/parallel.py
+++ sphinx/util/parallel.py
@@ -142,7 +142,7 @@ class ParallelTasks:
         return joined_any
 
 
-def make_chunks(arguments: Sequence[str], nproc: int, maxbatch: int = 10) -> List[Any]:
+def make_chunks(arguments: Sequence[str], nproc: int, maxbatch: int = 1000) -> List[Any]:
     # determine how many documents to read in one go
     nargs = len(arguments)
     chunksize = nargs // nproc
